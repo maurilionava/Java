3dec60cf6c2e53148cc735add9785604f95f0f8a
maurilionava <mnavajunior@gmail.com>
2021-07-09 20:37:59 -0300

    Add HelloWorld class
    Getting started with Java

---
+++ base/HelloWorld.java
@@ -0,0 +1,14 @@
+package base;
+
+/**
+ * A simple hello world class
+ * 
+ * @author  Maurilio Nava Junior
+ * @date    07/09/2021
+ */
+
+public class HelloWorld{    
+    public static void main(String[] args) {
+        System.out.println("Hello World Java");
+    }    
+}
